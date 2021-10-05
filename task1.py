a285ecbb811f40a8ae774b267fe65344be7be0b2
ethanDe <ethand971@deltalearns.ca>
2021-10-05 14:28:08 -0700

    Tasks

--- task1.py
+++ task1.py
@@ -17,3 +17,6 @@ example:
 Enter number:4
 4 8 12 16 20 24 28 32 36 40 44 48
 """
+
+mult = int(input("Enter an integer: "))
+print(mult, mult*2, mult*3, mult*4, mult*5, mult*6, mult*7, mult*8, mult*9, mult*10, mult*11, mult*12)
